cd8fe7501ea9ddfc592846a021fb0bafe237f13e
Claudio Rivera <claudio.rivera.sh@gmail.com>
2018-07-24 21:39:08 -0400

    ADD: ahow, create index

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,28 @@
 class TodosController < ApplicationController
   def index
-    @todos = todos.all
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to todos_path
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def edit
+    
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get 'todos', to: 'todos#index'
+  get 'todos/new', to: 'todos#new'
+  post 'todos', to: 'todos#create'
+  get 'todos/:id', to: 'todos#show', as: 'todo'
+  get 'todos/:id/edit', to: 'todos#edit'
+
+  root 'todos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
